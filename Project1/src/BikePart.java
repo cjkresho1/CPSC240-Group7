df1b73165a1873f4b431d95b3aaf9e3a1ff9a84a
cjkresho1 <cjkresho@gmail.com>
2020-02-16 10:44:38 -0500

    Update BikePart.java
    Cleaned up, added getPrice and toString

--- Project1/src/BikePart.java
+++ Project1/src/BikePart.java
@@ -1,21 +1,14 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
-
-public class BikePart {
+public class BikePart
+{
     /**
      * Constructs a BikePart object
      *
-     * @param name the name of the bike
-     * @param number the serial number of the bike
-     * @param listPrice the list price of the bike
-     * @param salesPrice the price of the bike if it is on sale
-     * @param sale whether the bike is on sale or not
+     * @param name       the name of the bike part
+     * @param number     the serial number of the bike part
+     * @param listPrice  the list price of the bike part
+     * @param salesPrice the price of the bike part if it is on sale
+     * @param sale       whether the bike part is on sale or not
+     * @param quantity   the quantity of bike parts
      * @author Anna Totten
      */
     String name;
@@ -25,7 +18,8 @@ public class BikePart {
     boolean sale;
     int quantity;
 
-    public BikePart(String name, int number, double listPrice, double salesPrice, boolean sale, int quantity) {
+    public BikePart(String name, int number, double listPrice, double salesPrice, boolean sale, int quantity)
+    {
         this.name = name;
         this.number = number;
         this.listPrice = listPrice;
@@ -35,66 +29,146 @@ public class BikePart {
     }
 
     /**
-     *
+     * Return the name of the bike
      * @return the name of the bike
      */
-    public String getName() {
+    public String getName()
+    {
         return this.name;
     }
 
-    public void setName(String newName) {
+    /**
+     * Set the name
+     * @param newName name to be set
+     */
+    public void setName(String newName)
+    {
         this.name = newName;
     }
 
     /**
-     *
+     * Get the bike number
      * @return the serial number of the bike
      */
-    public int getNumber() {
+    public int getNumber()
+    {
         return this.number;
     }
 
-    public void setNumber(int newNumber) {
+    /**
+     * Set the bike number
+     * @param newNumber number to be set
+     */
+    public void setNumber(int newNumber)
+    {
         this.number = newNumber;
     }
 
     /**
-     *
+     * Get the list price
      * @return the list price of the bike
      */
-    public double getListPrice() {
+    public double getListPrice()
+    {
         return this.listPrice;
     }
 
-    public void setListPrice(double newListPrice) {
+    /**
+     * Set the list price
+     * @param newListPrice list price to be set
+     */
+    public void setListPrice(double newListPrice)
+    {
         this.listPrice = newListPrice;
     }
 
     /**
-     *
+     * Get the sale price of the bike
      * @return the price of the bike if it is on sale
      */
-    public double getSalesPrice() {
+    public double getSalesPrice()
+    {
         return this.salesPrice;
     }
 
-    public void setSalesPrice(double newSalesPrice) {
+    /**
+     * Set the sale price of the bike
+     * @param newSalesPrice
+     */
+    public void setSalesPrice(double newSalesPrice)
+    {
         this.salesPrice = newSalesPrice;
     }
 
     /**
-     *
+     * Get if the bike is on sale
      * @return whether the bike is on sale or not
      */
-    public boolean getSale() {
+    public boolean getSale()
+    {
         return this.sale;
     }
 
-    public void setSale(boolean newSale) {
+    /**
+     * Set if the bike is on sale
+     * @param newSale the sale status to be set
+     */
+    public void setSale(boolean newSale)
+    {
         this.sale = newSale;
     }
 
-    public int getQuantity() { return this.quantity; }
+    /**
+     * Get the quantity of bikes
+     * @return The quantity of bikes
+     */
+    public int getQuantity()
+    {
+        return this.quantity;
+    }
 
-    public void setQuantity(int newQuantity) { this.quantity = newQuantity; }
+    /**
+     * Set the quantity of bikes
+     * @param newQuantity The quantity of bikes to be set
+     */
+    public void setQuantity(int newQuantity)
+    {
+        this.quantity = newQuantity;
+    }
+    
+    /**
+     * Get the current price of the bike part, based on the sale status
+     * @return The current price of the bike.
+     */
+    public double getPrice()
+    {
+        if (sale)
+        {
+            return salesPrice;
+        }
+        
+        return listPrice;
+    }
+    
+    /**
+     * Prints a String representation of the BikePart in the following format:
+     * names,number,listPrice,salesPrice,sale,quantity
+     * @return A string representation of the BikePart
+     */
+    public String toString()
+    {
+        String val = String.format("%s,%d,%.2f,%.2f,", name, number, listPrice, salesPrice);
+        
+        if (sale)
+        {
+            val = val + "true";
+        }
+        else
+        {
+            val = val + "false";
+        }
+        
+        val = val + "," + quantity;
+        return val;
+    }
 }
