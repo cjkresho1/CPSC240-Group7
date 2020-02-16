0e093b5fdc2359e2fb429fc524d5fa0eef2fba60
cjkresho1 <cjkresho@gmail.com>
2020-02-16 10:48:45 -0500

    Update IntegerComparator.java
    Added javadoc

--- Project1/src/IntegerComparator.java
+++ Project1/src/IntegerComparator.java
@@ -1,12 +1,18 @@
 import java.util.Comparator;
 
-public class IntegerComparator implements Comparator<BikePart> {
+/**
+ * Implements a comparator by BikePart number for the BikePart class
+ * @author Libbie
+ *
+ */
+public class IntegerComparator implements Comparator<BikePart>
+{
 
-	@Override
-	public int compare(BikePart o1, BikePart o2) {
-		return (o1.getNumber()-o2.getNumber());
-		
-	}
-	
+    @Override
+    public int compare(BikePart o1, BikePart o2)
+    {
+        return (o1.getNumber() - o2.getNumber());
+
+    }
 
 }
