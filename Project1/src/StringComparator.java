8a924665505448fc3653bb4b1eff5b902bb5a6f3
cjkresho1 <cjkresho@gmail.com>
2020-02-16 10:49:21 -0500

    Update StringComparator.java
    Added javadoc

--- Project1/src/StringComparator.java
+++ Project1/src/StringComparator.java
@@ -1,12 +1,18 @@
 import java.util.Comparator;
 
-public class StringComparator implements Comparator<BikePart> {
+/**
+ * Implements a comparator by BikePart name for the BikePart class
+ * @author Libbie
+ *
+ */
+public class StringComparator implements Comparator<BikePart>
+{
 
-	@Override
-	public int compare(BikePart o1, BikePart o2) {
-		return o1.getName().compareTo(o2.getName());
-		
-	}
-	
+    @Override
+    public int compare(BikePart o1, BikePart o2)
+    {
+        return o1.getName().compareTo(o2.getName());
+
+    }
 
 }
