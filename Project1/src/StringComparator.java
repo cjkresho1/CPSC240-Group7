2eec8cce33e7d5b27532fb5334a0a89032e19029
cjkresho1 <cjkresho@gmail.com>
2020-02-16 12:48:48 -0500

    Update StringComparator.java
    Changed to compareToIgnoreCase

--- Project1/src/StringComparator.java
+++ Project1/src/StringComparator.java
@@ -11,7 +11,7 @@ public class StringComparator implements Comparator<BikePart>
     @Override
     public int compare(BikePart o1, BikePart o2)
     {
-        return o1.getName().compareTo(o2.getName());
+        return o1.getName().compareToIgnoreCase(o2.getName());
 
     }
 
