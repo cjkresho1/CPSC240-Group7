ca203ec541ecae0c85f10d0e9b755f64fa406a5f
cjkresho1 <37028133+cjkresho1@users.noreply.github.com>
2020-03-14 09:38:41 -0400

    Main now has a complete project flow, from start to quit

--- Project2/src/Main.java
+++ Project2/src/Main.java
@@ -2,18 +2,21 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.*;
-import java.util.Date;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
- * Provides the main control structure for the project. Contains an ArrayList that acts as the warehouse for bike parts.
+ * Provides the main control structure for the project. Contains the main Warehouse
+ * and a list of Van Warehouses, and handles all user I/O.
  * @author Charles, Libbie, Anna Totten
  *
  */
 public class Main
 {
 	private static final String MAIN_NAME = "mainWarehouse";
-    private static final String DB_FILE_NAME = "warehouseDB.txt";
+    private static final String DB_FILE_NAME = "mainWarehouse.warehouse.txt";
+    private static final String FILE_MODIFIER = "warehouse.txt";
     private Warehouse mainWarehouse;
     private ArrayList<Warehouse> vans;
     private Scanner userIn;
@@ -24,6 +27,8 @@ public class Main
                 "display",
                 "sortname",
                 "sortnumber",
+                "addvan",
+                "transfer",
                 "quit"};
     
     /**
@@ -31,7 +36,10 @@ public class Main
      */
     public void run()
     {
-        //TODO finish run()
+    	mainWarehouse = new Warehouse(MAIN_NAME);
+    	vans = new ArrayList<Warehouse>();
+    	userIn = new Scanner(System.in);
+    	run = true;
     	
     	/*
     	 * Import existing Warehouse and Vans from files.
@@ -40,8 +48,107 @@ public class Main
     	 * Delete files after import.
     	 */
     	
-    	//Enter control loop, until user chooses to exit
+    	importData();
     	
+    	//Enter control loop, until user chooses to exit
+    	while(run)
+    	{
+    		displayUI();
+    		System.out.print("\n");
+    	}
+    	
+    	userIn.close();
+    }
+    
+    /**
+     * Helper method to run(). Imports existing data from text files before run.
+     */
+    private void importData()
+    {
+    	/*
+    	 * IMPORTANT: This string contains the absolute file path to the directory
+    	 * the application was run in. Use it to walk the existing files in the 
+    	 * directory.
+    	 * 
+    	 * The following code creates a directory at the path, and gets a list of all
+    	 * files/directories in it. 
+    	 */
+    	String absolutePath = System.getProperty("user.dir");
+    	
+    	File curDirectory = new File(absolutePath);
+    	File[] files = null;
+    	
+    	if (curDirectory.isDirectory())
+    	{
+    		files = curDirectory.listFiles();
+    	}
+    	
+    	if (files == null)
+    	{
+    		System.err.println("Something went wrong reading the file structure.");
+    		System.exit(1);
+    	}
+    	
+    	//Get all .warehouse.txt files from the current directory, then create
+    	//Warehouse objects from them
+    	
+    	for (File curFile : files)
+    	{
+    		if (curFile.exists())
+    		{
+    			if (curFile.getName().contains(FILE_MODIFIER))
+    			{
+    				//File is a warehouse. Let's get that data
+    				
+    				/*
+    				 * This gets the name of the Warehouse. How does this work?
+    				 * A .warehouse.txt file has the following format dictated by the
+    				 * quit() method: "[warehouseName].warehouse.txt"
+    				 * This means that we can take a substring of the file name to 
+    				 * remove the trailing 14 characters, which will be the 
+    				 * ".warehouse.txt", leaving only [warehouesName].
+    				 */
+    				
+    				String fileName = curFile.getName();
+    				fileName = fileName.substring(0, fileName.length() - 14);
+    				Warehouse curWarehouse = new Warehouse(fileName);
+    				
+    				try
+    				{
+    					Scanner curScan = new Scanner(curFile);
+    					while(curScan.hasNext())
+    					{
+    						String line = curScan.next();
+    						String[] pv = line.split(",");
+    						//Create new bike part from the ',' delimited line
+    		                BikePart bp = new BikePart(pv[0], Integer.parseInt(pv[1]), Double.parseDouble(pv[2]),
+    		                        Double.parseDouble(pv[3]), Boolean.parseBoolean(pv[4]), Integer.parseInt(pv[5]));
+    		                
+    		                curWarehouse.add(bp);
+    					}
+    					
+    					curScan.close();
+    				}
+    				catch (FileNotFoundException e)
+    				{
+    					System.err.println("Somehow, the DB file that just existed "
+    							+ "doesn't exist anymore.");
+    					e.printStackTrace();
+    				}
+    				
+    				if (curWarehouse.getName().equals(MAIN_NAME))
+    				{
+    					mainWarehouse = curWarehouse;
+    				}
+    				else
+    				{
+    					vans.add(curWarehouse);
+    				}
+    				
+    				curFile.delete();
+    			}
+    		}
+    	}
     }
 
     /**
@@ -147,6 +254,21 @@ public class Main
     }
     
     /**
+     * Transfer inventory between the warehouses and sales vans. Should use the 
+     * helper methods transferFromMain() and transferFromVan().
+     */
+    private void transfer()
+    {
+    	//TODO finish transfer()
+    	
+    	//Get transfer filename
+    	
+    	//read first line, determine if it's a transfer from the mainWarehouse or a 
+    	//sales van. Call appropriate helper method.
+    	
+    }
+    
+    /**
      * Transfer parts between the mainWarehouse to a Van. Uses a text file.
      * 
      * First line of the transfer file should be:
@@ -206,13 +328,81 @@ public class Main
      */
     private void quit()
     {
-    	//TODO finish quit()
-    	
     	/*
     	 * On exit, write existing Warehouse and Vans to files.
     	 * Warehouse file to be: "warehouseDB.txt", specified by DB_FILE_NAME
     	 * Van files to be: "Van" + [van number] EX: Van1, Van2, Van3...Van[n] for n vans
     	 */
+    	
+    	//Create database file for the mainWarehouse
+    	File mainWarehouseFile = new File(DB_FILE_NAME);
+    	try
+        {
+            if (mainWarehouseFile.exists())
+            {
+            	mainWarehouseFile.delete();
+            }
+            
+            mainWarehouseFile.createNewFile();
+            FileWriter dbWriter = new FileWriter(mainWarehouseFile);
+            
+            for(BikePart part : mainWarehouse.getInventory())
+            {
+                dbWriter.write(part.toString() + "\n");
+            }
+            
+            dbWriter.close();
+        }
+        catch(FileNotFoundException e)
+        {
+            System.err.println("Warehouse database file couldn't be created! "
+            		+ "Data will be lost!");
+            e.printStackTrace();
+        }
+        catch(IOException e)
+        {
+            System.err.println("Couldn't write to the warehouse database file, "
+            		+ "data will be lost!");
+            e.printStackTrace();
+        }
+    	
+    	//Create database files for the vans. ENSURE NO 2 VANS HAVE THE SAME NAME
+    	for (int i = 0; i < vans.size(); i++)
+    	{
+    		Warehouse curVan = vans.get(i);
+    		File vanFile = new File(curVan.getName() + FILE_MODIFIER);
+        	try
+            {
+                if (vanFile.exists())
+                {
+                	vanFile.delete();
+                }
+                
+                vanFile.createNewFile();
+                FileWriter vanWriter = new FileWriter(vanFile);
+                
+                for(BikePart part : curVan.getInventory())
+                {
+                	vanWriter.write(part.toString() + "\n");
+                }
+                
+                vanWriter.close();
+            }
+            catch(FileNotFoundException e)
+            {
+                System.err.println("Van " + curVan.getName() +" database file "
+                		+ "couldn't be created! Data will be lost!");
+                e.printStackTrace();
+            }
+            catch(IOException e)
+            {
+                System.err.println("Couldn't write to van " + curVan.getName() + 
+                		" database file, data will be lost!");
+                e.printStackTrace();
+            }
+    	}
+    	
+    	run = false;
     }
 
     /**
@@ -220,13 +410,65 @@ public class Main
      */
     private void displayUI()
     {
-    	//TODO finish displayUI()
-    	
-    	//Print options menu, and get user input
-    	
-    	/*
-    	 * On user input, switch on the specified option, calling the proper method,
-    	 * then exit this method
-    	 */
+    	// Print out user options
+        System.out.println(
+                "Please select your option from the following menu:\n" + 
+                "Read: Read an inventory delivery file\n" + 
+                "Enter: Enter a part\n" + 
+                "Sell: Sell a part\n" + 
+                "Display: Display a part\n" + 
+                "SortName: Sort parts by part name\n" + 
+                "SortNumber: Sort parts by part number\n" + 
+                "AddVan: Add a new sales van to the fleet\n" +
+                "Transfer: Read a inventory transfer file\n" +
+                "Quit: Close the program\n" + 
+                "Enter your choice:"
+                );
+        // Get user input
+        String input = userIn.next();
+        int choice = -1;
+        for (int i = 0; i < CHOICES.length; i++)
+        {
+            if (CHOICES[i].equalsIgnoreCase(input))
+            {
+                choice = i;
+                break;
+            }
+        }
+        System.out.print("\n");
+        // Call respective method
+        switch(choice)
+        {
+        case 0:
+            read();
+            break;
+        case 1: 
+            add();
+            break;
+        case 2:
+            sell();
+            break;
+        case 3: 
+            display();
+            break;
+        case 4:
+            sortName();
+            break;
+        case 5:
+            sortNumber();
+            break;
+        case 6:
+            addVan();
+            break;
+        case 7: 
+        	transfer();
+        	break;
+        case 8:
+        	quit();
+        	break;
+        default:
+            System.out.println("That didn't match any option, please try again.");
+            break;
+        }
     }
 }
