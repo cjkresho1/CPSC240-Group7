4427403b8b8724d9b9dc8d406de7ba69b9af78a0
cjkresho1 <cjkresho@gmail.com>
2020-03-18 16:17:09 -0400

    Update Main.java

--- Project2/src/Main.java
+++ Project2/src/Main.java
@@ -2,7 +2,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Date;
